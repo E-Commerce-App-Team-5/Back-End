b03484ac1668887409f00019a6a0ebaf43b027b9
Nur Fatchurohman <78410989+FaturFawkes@users.noreply.github.com>
2022-10-25 19:33:42 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	// "github.com/joho/godotenv"
-	"github.com/joho/godotenv"
 	"github.com/labstack/gommon/log"
 )
 
@@ -31,11 +30,11 @@ func NewConfig() *AppConfig {
 func initConfig() *AppConfig {
 	var app AppConfig
 
-	err := godotenv.Load("config.env")
-	if err != nil {
-		log.Error("config error :", err.Error())
-		return nil
-	}
+	// err := godotenv.Load("config.env")
+	// if err != nil {
+	//	log.Error("config error :", err.Error())
+	//	return nil
+	// }
 
 	app.DBUser = os.Getenv("DB_USER")
 	app.DBPwd = os.Getenv("DB_PWD")
